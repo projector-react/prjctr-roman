8f10fec0f86201c871e732eba4d43b31cee8ba19
iamr0man <koriny4@gmail.com>
2022-07-04 10:26:33 +0300

    added object wrapper for filter params service;

--- src/services/filterParams.ts
+++ src/services/filterParams.ts
@@ -2,13 +2,17 @@ import React from "react";
 import { makeAutoObservable } from "mobx";
 import { Category, Direction, Format, Level } from "../types/filter";
 
-export interface FilterParamsState {
+export interface FilterParamsProps {
     readonly category: Category,
     readonly direction: Direction,
     readonly format: Format,
     readonly level: Level,
     readonly query: string,
     readonly page: number
+}
+
+export interface FilterParamsState {
+    state: FilterParamsProps
 }
 
 export interface FilterParamsActions {
@@ -28,12 +32,14 @@ export function createFilterParamsContext (filterParamsService: FilterParamsServ
 }
 
 export default class FilterParams implements FilterParamsService {
-    category: Category = Category.ALL
-    direction: Direction = Direction.ALL
-    format: Format = Format.ALL
-    level: Level = Level.ANY
-    query: string = ''
-    page: number = 1
+    state = {
+        category: Category.ALL,
+        direction: Direction.ALL,
+        format: Format.ALL,
+        level: Level.ANY,
+        query: '',
+        page: 1
+    }
 
     constructor() {
         makeAutoObservable(this);
@@ -41,35 +47,55 @@ export default class FilterParams implements FilterParamsService {
 
     setCategory = (category: Category) => {
         this.reset();
-        this.category = category
+        this.state = {
+            ...this.state,
+            category
+        }
     };
 
     setDirection = (direction: Direction) => {
-        this.direction = direction
+        this.state = {
+            ...this.state,
+            direction
+        }
     };
 
     setFormat = (format: Format) => {
-        this.format = format
+        this.state = {
+            ...this.state,
+            format
+        }
     }
 
     setLevel = (level: Level) => {
-        this.level = level
+        this.state = {
+            ...this.state,
+            level
+        }
     }
 
     setQuery = (query: string) => {
-        this.query = query
+        this.state = {
+            ...this.state,
+            query
+        }
     };
 
     setPage = (page: number) => {
-        this.page = page
+        this.state = {
+            ...this.state,
+            page
+        }
     };
 
     reset = () => {
-        this.category = Category.ALL
-        this.direction = Direction.ALL
-        this.format = Format.ALL
-        this.level = Level.ANY
-        this.query = ''
-        this.page = 1
+        this.state = {
+            category: Category.ALL,
+            direction: Direction.ALL,
+            format: Format.ALL,
+            level: Level.ANY,
+            query: '',
+            page: 1
+        }
     };
 }
