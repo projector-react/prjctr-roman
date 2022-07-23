69c6f8d63e58f7d6cfa601dbed2b21664bb2f746
iamr0man <koriny4@gmail.com>
2022-07-23 10:37:21 +0300

    added auth services to composition-root;

--- src/composition-root.tsx
+++ src/composition-root.tsx
@@ -4,6 +4,15 @@ import { createAxiosInstance } from "./services/api/create-axios-instance";
 import { ApiService } from "./services/api/api";
 import type { IApiService } from "./services/api/api";
 
+import { AuthService } from "./services/auth/auth";
+import type { IAuthService } from "./services/auth/auth";
+import { UserAuthService } from "./services/auth/user";
+import type { IUserAuthService } from "./services/auth/user";
+import { SignUpService } from "./services/auth/signUp";
+import type { ISignUpService } from "./services/auth/signUp";
+import { SignInService } from "./services/auth/signIn";
+import type { ISignInService } from "./services/auth/signIn";
+
 import FilterParams from "./services/filterParams";
 import type { FilterParamsService } from "./services/filterParams";
 
@@ -15,12 +24,21 @@ import type { FilterService } from "./services/filter";
 
 import { createFilterViewModel } from "./components/VideoLibrary/Filter";
 import { createVideoViewModel } from "./components/VideoLibrary/Video";
+import { createAuthViewModel } from "./components/Auth";
 
 import { TYPES } from "./constants";
 
 export default function createCompositionRoot (container: interfaces.Container) {
-    container.bind(TYPES.filterViewModel).toDynamicValue(() => createAxiosInstance())
-    container.bind<IApiService>(TYPES.apiService).to(ApiService)
+    container.bind(TYPES.axiosInstance).toDynamicValue(() => createAxiosInstance())
+    container.bind<IApiService>(TYPES.apiService).to(ApiService).inSingletonScope()
+
+    container.bind<IAuthService>(TYPES.authService).to(AuthService).inSingletonScope()
+    container.bind<IUserAuthService>(TYPES.userAuthService).to(UserAuthService).inSingletonScope()
+
+    container.bind<ISignUpService>(TYPES.signUpService).to(SignUpService).inSingletonScope()
+    container.bind<ISignInService>(TYPES.signInService).to(SignInService).inSingletonScope()
+
+    container.bind(TYPES.authViewModel).toDynamicValue(({ container }) => createAuthViewModel(container.get(TYPES.authService), container.get(TYPES.userAuthService), container.get(TYPES.signUpService), container.get(TYPES.signInService)))
 
     container.bind<FilterParamsService>(TYPES.filterParams).to(FilterParams).inSingletonScope()
     container.bind(TYPES.filterViewModel).toDynamicValue(({ container }) => createFilterViewModel(container.get(TYPES.filterParams)))
@@ -28,6 +46,6 @@ export default function createCompositionRoot (container: interfaces.Container) 
     container.bind<FilterResultService>(TYPES.filterResult).to(FilterResult).inSingletonScope()
     container.bind(TYPES.videoViewModel).toDynamicValue(({ container }) => createVideoViewModel(container.get(TYPES.filterResult)))
 
-    container.bind<FilterService>(TYPES.filterService).to(Filter)
+    container.bind<FilterService>(TYPES.filterService).to(Filter).inSingletonScope()
     return container
 }
